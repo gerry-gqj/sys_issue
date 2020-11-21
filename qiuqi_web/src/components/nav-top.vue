cebde37388d041d4dee504ce2b3db451fd869c87
A-Guo15 <g_qj0115@163.cm>
2020-11-21 13:33:42 +0800

    #退出登录实现

--- qiuqi_web/src/components/nav-top.vue
+++ qiuqi_web/src/components/nav-top.vue
@@ -10,22 +10,46 @@
     <h1>GBA Issue管理系统</h1>
     <el-aside width="auto"
               class="header-logo tap">
-      <el-avatar icon="el-icon-user-solid"
-                 class="headerLogo"></el-avatar>
+      <el-dropdown><span>
+          <a class="user"
+             @click="ModifyUserInformation"><span class="userimg">
+              <el-avatar icon="el-icon-user-solid"
+                         class="headerLogo"></el-avatar>
+            </span>
+            <span class="username">{{user}}</span>
+          </a></span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>修改用户信息</el-dropdown-item>
+          <el-dropdown-item @click.native="logout"
+                            divided>退出登录</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+
     </el-aside>
   </el-container>
 </template>
 
 <script>
+import router from '@/router';
 export default {
   data () {
+
     return {
+      user: 'user',
       activeIndex: "1",
     };
   },
   methods: {
-    handleSelect (key, keyPath) {
-      console.log(key, keyPath);
+    // handleSelect (key, keyPath) {
+    //   console.log(key, keyPath);
+    // },
+    ModifyUserInformation () {
+      this.user = 'User';
+    },
+    logout () {
+      // this.$router.push('/');
+      // 实现页面跳转和刷新页面
+      window.location.href = "/";
     },
   },
 };
@@ -54,5 +78,35 @@ section {
 .headerLogo,
 .logo {
   cursor: pointer;
+}
+
+.user {
+  font-size: 12px;
+  min-height: 31px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  /* height: 20px; */
+  margin-top: 5px;
+  color: #202046;
+  white-space: normal;
+}
+/* .username {
+  text-align: left;
+  font-size: 10px;
+  text-overflow: -o-ellipsis-lastline;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+} */
+
+.el-dropdown-link {
+  cursor: pointer;
+  color: #409eff;
+}
+.el-icon-arrow-down {
+  font-size: 12px;
 }
 </style>
