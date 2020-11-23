8bc147067430cf7d08a335304806af3d13c876e6
A-Guo15 <g_qj0115@163.cm>
2020-11-23 22:28:44 +0800

    #修改页面路由

--- qiuqi_web/src/main.ts
+++ qiuqi_web/src/main.ts
@@ -1,11 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from './store/';
+import store from './store';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import axios from 'axios'
-import Qs from 'qs'
+import axios from 'axios';
+import Qs from 'qs';
 Vue.prototype.$qs = Qs
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false;
