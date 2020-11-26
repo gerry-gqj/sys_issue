013e656a830eed4da7221488d0ed53f18b466211
Your Name <you@example.com>
2020-11-26 10:20:37 +0800

    #添加welcome页面

--- qiuqi_web/src/views/main/welcome.vue
+++ qiuqi_web/src/views/main/welcome.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-    <h1>欢迎回来,大傻纸!!!{{}}</h1>
+    <h1>欢迎回来，{{ user }}<br />您的身份是：{{ role }}</h1>
+    <p>{{ currentTime }}</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      role: "",
+      user: "",
+      currentTime: "",
+    };
+  },
+  mounted() {
+    this.role = localStorage.getItem("role");
+    // this.name = localStorage.gettItem("username");
+    this.user = localStorage.getItem("username");
+    // let _this = this; // 声明一个变量指向Vue实例this，保证作用域一致
+    // this.timer = setInterval(() => {
+    //   _this.date = new Date(); // 修改数据date
+    // }, 1000)
+  },
+  //  beforeDestroy() {
+  //   if (this.timer) {
+  //     clearInterval(this.timer); // 在Vue实例销毁前，清除我们的定时器
+  //   }
+  // }
+};
 </script>
-
+  
 <style>
 </style>
